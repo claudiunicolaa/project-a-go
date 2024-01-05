f95f3b2a576b0dfe1fcfb71a75970a2750e2416c
Claudiu <claudiunicola96@gmail.com>
2024-01-05 16:46:23 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/claudiunicolaa/project-a-go
+
+go 1.21.5
+
+require github.com/claudiunicolaa/project-b-go v0.0.0-20240105143918-d9fe48ace6d0
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	projectbgo "github.com/claudiunicolaa/project-b-go"
+)
+
+func main() {
+	fmt.Println("Hello from project a!")
+
+	projectbgo.SayHello()
+}
